fd89215f0b7cbf164b53d13b6ae60236bfc9655e
emilio-fv <emiliofv.sd@gmail.com>
2022-11-16 13:45:06 -0700

    Add dev links

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Inventory from './views/Inventory';
-
+import { Routes, Route } from 'react-router-dom';
+import DevMain from './views/DevMain';
 function App() {
   return (
-    <Inventory />
+    <Routes>
+      <Route path="/" element={ <DevMain />}/>
+      <Route path='/admin/inventory' element={ <Inventory /> }/>
+    </Routes>
   );
 }
 
---
+++ client/src/views/DevMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const DevMain = () => {
+    return (
+        <>
+            <Link to='/admin/inventory'>Inventory (Admin)</Link>
+        </>
+    )
+}
+
+export default DevMain;
