352db963540911ededa8e65f99dd72d9318db6bc
Lanceagleron <lanceagleron17@gmail.com>
2022-11-17 11:11:05 -0800

    add something

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import {Route, Routes} from 'react-router-dom';
 import Home from './views/Home';
 import Menu from './views/Menu';
@@ -12,17 +11,6 @@ function App() {
         <Route path='/menu' element={<Menu/>}/>
       </Routes>
     </div>
-=======
-import Inventory from './views/Inventory';
-import { Routes, Route } from 'react-router-dom';
-import DevMain from './views/DevMain';
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={ <DevMain />}/>
-      <Route path='/admin/inventory' element={ <Inventory /> }/>
-    </Routes>
->>>>>>> 65bf3a347aa6bd625958dffc500203f79a4906ec
   );
 }
 
